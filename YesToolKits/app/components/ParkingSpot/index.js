8fbcfa838233eeeb5b3dc540a09e28d40de4b11e
Bill <yeluyang530@163.com>
2018-09-18 13:45:16 +1000

    source changed

--- YesToolKits/app/components/ParkingSpot/index.js
+++ YesToolKits/app/components/ParkingSpot/index.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
- 
+import { View,Text } from 'react-native'
+import styles from './styles'
+import {Header,Left,Icon,Container,Content} from 'native-base'
 class ParkingSpot extends Component {
     render() {
+        console.log('222')
+        const {text1,view1} = styles
         return (
-        <View>
-            
-        </View>
+            <Container>
+            <Header>
+                <Left>
+                    <Icon name="ios-menu" onPress={()=>this.props.navigation.openDrawer()} />
+                </Left>
+                <Text>ParkingSpot</Text>
+            </Header>
+            <Content contentContainerStyle={view1}>
+                <Text>1234123</Text>
+            </Content>
+        </Container>
         )
     }
 }
---
+++ YesToolKits/app/components/ParkingSpot/styles.js
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native'
+
+export default StyleSheet.create({
+    view1: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1
+    },
+    text1: {
+        justifyContent: 'center'
+
+    }
+})
