5d2e05cc213777bd9411e4079738159e2037f416
Bill <yeluyang530@163.com>
2018-10-05 22:29:40 +1000

    Get URL of image from firebase

--- YesToolKits/app/components/InstaDiary/tabs/ListTab/index.js
+++ YesToolKits/app/components/InstaDiary/tabs/ListTab/index.js
@@ -1,11 +1,69 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View,Text,ScrollView, Button, Image, ActivityIndicator, TouchableOpacity,Dimensions,TextInput } from 'react-native'
 import styles from './styles'
 import {Header,Left,Icon,Container,Content,Body,Right} from 'native-base'
- 
+import * as firebase from 'firebase'
+import RNFetchBlob from 'react-native-fetch-blob'
+import axios from 'axios'
+
+// const config = {
+//     apiKey: "AIzaSyBtHCE5y0HZ-eB7uLleX-cgS8-oS_sjUF4",
+//     authDomain: "yestoolkits.firebaseapp.com",
+//     databaseURL: "https://yestoolkits.firebaseio.com",
+//     projectId: "yestoolkits",
+//     storageBucket: "yestoolkits.appspot.com",
+//     messagingSenderId: "304847695731"
+//   };
+//firebase.initializeApp(config);
+
 class ListTab extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            imageList: [],
+            loading: false,
+        }
+        this.downloadImages = this.downloadImages.bind(this)
+
+    }
+
+    downloadImages() {
+        if(this.state.imageList.length == 0) {
+            this.setState({ loading: true })
+            const Blob = RNFetchBlob.polyfill.Blob
+            const fs = RNFetchBlob.fs
+            window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
+            window.Blob = Blob
+            //const { uid } = this.state.user
+            const uid = "Images"
+            const imageRef = firebase.storage().ref(uid)
+            var bucket = imageRef.bucket
+            var firebaseUrl = "https://firebasestorage.googleapis.com/v0/b/" + bucket + "/o/";
+            console.log("Ref: "+imageRef.toString())
+            console.log("getURL: "+imageRef.getDownloadURL)
+            console.log("getMETA: "+imageRef.getMetadata)
+            var gsReference = firebase.storage().refFromURL(imageRef.toString())
+            console.log("gs: "+gsReference) 
+            console.log("Firebase URL " + firebaseUrl)
+
+            var bucket = firebase.storage().ref().bucket;
+            var firebaseUrl = "https://firebasestorage.googleapis.com/v0/b/" + bucket + "/o/";
+            var finalUrl = firebaseUrl + 'path%2Fto%2Fresource';
+            firebase.auth().signInAnonymously()
+            firebase.auth().currentUser.getToken()
+            .then((token) => {
+            fetch(finalUrl, {headers: {'Authorization' : 'Firebase ' + token}})
+            .then((response) => response.json())
+            .then((responseJson) => {
+                var downloadURL = finalUrl + "?alt=media&token=" + responseJson.downloadTokens})
+                console.log("downloadURL " + downloadURL)
+            })
+
+        }
+    }
+
     render() {
-        const {text1,view1,weather_icon,weather_view,weather_each} = styles
+        const {text1,view1} = styles
 
         return (
             
@@ -23,7 +81,7 @@ class ListTab extends Component {
                 
             </Header>
             <Content contentContainerStyle={view1}>
-                <Text>1111</Text>
+                <Button title={"get"} onPress={this.downloadImages}/>
                 
                 
 
