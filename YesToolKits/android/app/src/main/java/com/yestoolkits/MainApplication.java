ba52b9649268506c3105476e8436edc9a5ba27d5
Bill <yeluyang530@163.com>
2018-11-01 19:44:54 +1100

    Link pompt-android lib

--- YesToolKits/android/app/src/main/java/com/yestoolkits/MainApplication.java
+++ YesToolKits/android/app/src/main/java/com/yestoolkits/MainApplication.java
@@ -3,6 +3,7 @@ package com.yestoolkits;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import im.shimo.react.prompt.RNPromptPackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNPromptPackage(),
             new RNFetchBlobPackage(),
             new PickerPackage(),
             new VectorIconsPackage()
